f470b5adf6114f018a4f340a85c5238e90f6edbc
thedanielenrico <thedanielenrico@gmail.com>
2018-12-27 10:01:52 -0800

    Work on logging letters to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,32 @@
 // Random word list
 var wordList = ['ocean', 'pirate', "beach", "parrot", "ship"];
 var word = wordList[Math.floor(Math.random() * wordList.length)];
+var letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
+// Show letter tiles
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_ ";
 }
-// Show letter tiles
-// var remainingLetters = word.length;
+var remainingLetters = word.length;
+for (var j = 0; j < remainingLetters.length; j++) {
+
+}
+
 var currentWord = document.getElementById("currentWord");
-var lettersLeft = document.createElement("p");
-lettersLeft.textContent = answerArray;
-currentWord.appendChild(lettersLeft);
-console.log(word);
+var letterSpaces = document.createElement("p");
+letterSpaces.textContent = answerArray;
+currentWord.appendChild(letterSpaces);
+document.onkeyup = function (event) {
+    var keyPressed = event.key;
+    letterSpaces.textContent = answerArray;
+    if (word.includes(keyPressed)) {
+        letterSpaces.textContent = answerArray + keyPressed;
+    }
+}
 
-
+// if (key presssed is equal to a letter in the word) {
+//     update letter tiles
+// } else {
+//     log key pressed into the used letter section;
+// }
