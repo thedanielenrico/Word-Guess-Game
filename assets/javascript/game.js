e03393f185a9e1358014d28b53ad1cd681c77e17
thedanielenrico <thedanielenrico@gmail.com>
2019-01-02 19:10:57 -0800

    Add last word and losses for UI

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 // =================================================================
 // Global variables
 // =================================================================
-var wordList = ['ocean', 'pirate', "beach", "parrot", "ship"];
+var wordList = ['ocean', 'pirate', "beach", "parrot", "ship", "curse", "treasure", "map", "hook", "aye", "boom", "naval", "captain", "mate", "dolphin", "nautical", "arg", "atlantic", "pacific", "caribbean", "island", "cannon", "sword", "deck"];
 var word = wordList[Math.floor(Math.random() * wordList.length)];
 var wordArray = "";
 var rightLetter = [];
 var wrongLetter = [];
 var answerArray = [];
 var wins = 0;
+var losses = 0;
 var guessesTotal = 11;
 var guessesLeft = document.getElementById("guessesLeft");
 var regex = /[a-zA-Z]/;
@@ -35,9 +36,11 @@ function gameInit() {
     }
     document.getElementById("currentWord").textContent = answerArray.join(" ");
     document.getElementById("winTotal").textContent = wins;
+    document.getElementById("lossTotal").textContent = losses;
     document.getElementById("guessesLeft").textContent = guessesTotal;
     document.getElementById("wrongLetters").textContent = "";
     document.getElementById("oldWords").textContent = oldWords;
+    console.log(word);
 }
 // =================================================================
 // Game events
@@ -45,6 +48,7 @@ function gameInit() {
 document.body.onload = gameInit();
 document.onkeyup = function (event) {
     var guess = event.key;
+
     if ((guess.length == 1) && (regex.test(guess))) {
         while (wordArray.indexOf(guess) != -1) {
             rightLetter.push(guess);
@@ -57,22 +61,20 @@ document.onkeyup = function (event) {
             oldWords = word;
             gameInit();
             return
-
         }
         if (!word.includes(guess)) {
             if (!wrongLetter.includes(guess)) {
                 wrongLetter.push(guess);
                 wrongLetters.textContent = wrongLetter.join(" ");
                 guessesTotal--;
-
+                oldWords = word;
                 if (guessesTotal <= 0) {
+                    losses++;
                     gameInit();
                     return
                 }
                 document.getElementById("guessesLeft").textContent = guessesTotal;
             }
-        }
-        if (!word.includes(guess) || rightLetter.length === word.length) {
         }
     }
 }
